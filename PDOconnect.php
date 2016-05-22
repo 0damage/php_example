fff464a3d2248f62b63e1a8b8d6d1d9d804ff301
Rafael Shamilov <rafael_shamilov@mail.ru>
2016-05-22 14:56:51 +0300

    1.Начало

---
+++ PDOconnect.php
@@ -0,0 +1,13 @@
+﻿<?php 
+require_once 'logi.php';
+
+// подключение к базе данных через субд mysql расширение PDO
+$dsn="mysql:host=$host;dbname=$dbname";
+$opt=array(
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
+
+$pdo=new PDO ($dsn,$name,$pass,$opt);
+$pdo->exec('SET NAMES utf8');
+//____________________________________________________________________________________
---
+++ main.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html>
+<!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+	<title>PHP</title>
+        <meta name="description" content="" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!--Подключение совместимости с edge -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> <!-- Нужно для создания адаптивного дизайна -->
+        <link rel="shortcut icon" href="favicon.png" /> <!-- фавиконка -->
+        <link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" /> <!-- подключение бутстрап сетки -->
+        <link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" /> <!-- подключение иконок http://fontawesome.io/icons/ -->
+        <link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" /> <!-- менеджер попап кнопок fancybox (вспылающая форма,изображения и тд) -->
+	<link rel="stylesheet" href="css/fonts.css" /> <!-- шрифты по умолч для текста-->
+	<link rel="stylesheet" href="css/main.css" /> 
+	<link rel="stylesheet" href="css/media.css" />
+        <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Anonymous+Pro' rel='stylesheet' type='text/css'>        
+</head>
+<body>
+    <!--[if lt IE 9]>
+    <script src="libs/html5shiv/es5-shim.min.js"></script>
+    <script src="libs/html5shiv/html5shiv.min.js"></script>
+    <script src="libs/html5shiv/html5shiv-printshiv.min.js"></script>
+    <script src="libs/respond/respond.min.js"></script>
+    <![endif]-->
+    <script src="libs/jquery/jquery-1.11.1.min.js"></script> <!-- подключение jquery -->
+    <script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+</body>
+</html>
